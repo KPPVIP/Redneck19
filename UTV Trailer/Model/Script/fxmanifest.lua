6651b7ab318e9a3eecba2c9804c1cd0338191975
Guudz <Kilian1702@outlook.com>
2023-01-23 23:03:18 +0100

    Initial commit

---
+++ UTV Trailer/Model/Script/fxmanifest.lua
@@ -0,0 +1,11 @@
+<!-- Leaked By: Leaking Hub Joshua Y.#9153 Database https://discord.gg/KsNWjUJ -->
+fx_version 'adamant'
+games { 'gta5' }
+
+description 'This script allows the gator to be attached to the trailer.'
+made_by 'Redneck Modifications Team'
+website 'www.redneckmods.com'
+
+client_scripts {
+	'client.lua'
+}
